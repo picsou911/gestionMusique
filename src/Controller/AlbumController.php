0c237da38018d374848cf63f43846712d69f9ff0
Jean-Paul Beaudonnet <jpjp03911@gmail.com>
2023-11-15 10:07:04 +0100

    gestion du findBy

--- src/Controller/AlbumController.php
+++ src/Controller/AlbumController.php
@@ -14,20 +14,12 @@ class AlbumController extends AbstractController
     #[Route('/albums', name: 'albums', methods: ['GET'])]
     public function listeAlbums(AlbumRepository $repo)
     {
-        $albums=$repo->findAll();
+        $albums=$repo->findBy(['date'=>'2006'], ['nom'=>'asc'],5);
         return $this->render('album/listeAlbums.html.twig', [
             'lesAlbums' => $albums
         ]);
     }
 
-    //#[Route('/album/{id}', name: 'ficheAlbum', methods: ['GET'])]
-    //public function ficheAlbum($id, AlbumRepository $repo): Response
-    //{
-    //    $album= $repo->find($id);
-    //    return $this->render('album/ficheAlbum.html.twig', [
-    //        'lealbum' => $album
-    //    ]);
-    //}
     #[Route('/album/{id}', name: 'ficheAlbum', methods: ['GET'])]
   
     public function fichealbum(AlbumRepository $albumRepository, $id)
